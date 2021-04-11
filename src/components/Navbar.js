df5d3ec183322419e0147f1e90db69d80ea1adda
Roayacom <77238762+Roayacom@users.noreply.github.com>
2021-04-12 01:05:08 +0300

    second

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,13 @@ import Logo from './Logo'
 function Navbar() {
     return (
         <div>
-            <nav class="flex items-center justify-between bg-browngray py-1 pr-10 pt-2">
-                <div class="flex items-center">
+            <nav className="flex items-center justify-between bg-browngray py-1 pr-10 pt-2">
+                <div className="flex items-center">
                   <Logo/>
 
 
                     <div id="nav-content"
-                        class="hidden lg:flex md:flex content-start space-x-6 lg:block md:block p-4 mx-12">
+                        className="hidden lg:flex md:flex content-start space-x-6 lg:block md:block p-4 mx-12">
                      <Link to="/">
                             <Button type="Menu" text="Home"/>
                         </Link>
@@ -26,7 +26,7 @@ function Navbar() {
                     </div>
                 </div>
 
-                <div class="flex space-x-4 ">
+                <div className="flex space-x-4 ">
 
 
                     <Button text="Login" type="Dark" />
@@ -35,9 +35,9 @@ function Navbar() {
 
 
 
-                    <div class=" block lg:hidden md:hidden pt-2">
-                        <button id="nav-toggle" type="button" class="text-white">
-                            <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                    <div className=" block lg:hidden md:hidden pt-2">
+                        <button id="nav-toggle" type="button" className="text-white">
+                            <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
                                 <path
                                     d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 17h-12v-2h12v2zm0-4h-12v-2h12v2zm0-4h-12v-2h12v2z" />
                             </svg>
